ade1aa304f91bb1fad993a7a35cdc6f6bae59168
Andrew <andrew.a.vallejo@gmail.com>
2023-11-04 19:19:57 -0600

    Add carousel logic

--- src/app/components/Wheel.tsx
+++ src/app/components/Wheel.tsx
@@ -1,38 +1,66 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 
-import Card from "./Card";
+import Type from "./Type";
 
-import styles from "./wheel.module.scss";
-import { getCenterOfElement } from "../../utility/math";
+import { getCenterOfElement, multiplyByPi } from "utility/math";
+import pokemonTypes from "utility/types";
+
+import styles from "./Wheel.module.scss";
+
+interface WheelProps {
+  Types: any[];
+}
 
 const initialWheel = {
-  radius: 1,
+  radius: 250,
   center: { x: 0, y: 0 },
   theta: 0,
+  animationId: null,
 };
 
 export default function Wheel() {
   const [wheel, setWheel] = useState(initialWheel);
-
   const wheelRef = useRef(null);
 
   useEffect(() => {
     if (wheelRef.current) {
       const centerOfWheel = getCenterOfElement(wheelRef.current);
-      $: console.log("centerOfWheel", centerOfWheel);
-
       setWheel((prevWheel) => ({ ...prevWheel, center: centerOfWheel }));
     }
   }, [wheelRef]);
 
+  useEffect(() => {
+    if (wheelRef.current) {
+      wheelRef.current.style.transform = `rotate(${wheel.theta}deg)`;
+    }
+  }, [wheel.theta]);
+
+  const handleScroll = (event) => {
+    const wheelTheta = wheel.theta + event.deltaY * 0.07;
+
+    if (wheel.animationId) {
+      cancelAnimationFrame(wheel.animationId);
+    }
+
+    const animationId = requestAnimationFrame(() => {
+      setWheel((prevWheel) => ({ ...prevWheel, theta: wheelTheta }));
+    });
+
+    setWheel((prevWheel) => ({ ...prevWheel, animationId }));
+  };
+
   return (
-    <div ref={wheelRef} className={styles.wheel}>
-      <Card
-        title="Effectiveness Chart"
-        center={wheel.center}
-        radius={wheel.radius}
-        theta={wheel.theta}
-      />
+    <div onWheel={handleScroll} ref={wheelRef} className={styles.wheel}>
+      {pokemonTypes.map((type, index) => (
+        <Type
+          key={index}
+          title="Effectiveness Chart"
+          type={type}
+          center={wheel.center}
+          radius={wheel.radius}
+          theta={multiplyByPi(index)}
+        />
+      ))}
     </div>
   );
 }
