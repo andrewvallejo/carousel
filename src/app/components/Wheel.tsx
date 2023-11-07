6301ca36ab23bf3fb964a5725a88d48391ec597d
Andrew <andrew.a.vallejo@gmail.com>
2023-11-06 20:05:29 -0700

    Revert "Add snap logic"
    This reverts commit 1689362c55c8d7604aeb2114000180572a43ec91.

--- src/app/components/Wheel.tsx
+++ src/app/components/Wheel.tsx
@@ -38,16 +38,14 @@ const initialWheelState: WheelState = {
 export function Wheel() {
   const [wheel, setWheel] = useState(initialWheelState);
   const [loaded, setLoaded] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
 
   const wheelStateRef = useRef(wheel);
   const wheelRef = useRef(null);
-  const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
     if (!wheelRef.current) return;
     const { width, height } = wheelRef.current.getBoundingClientRect();
-    const radius = Math.min(width, height) / 1.5;
+    const radius = Math.min(width, height) / 2;
     const center = { x: width / 2, y: height / 2 };
     setWheel((prevWheel) => ({ ...prevWheel, center, radius }));
     setLoaded(true);
@@ -64,124 +62,25 @@ export function Wheel() {
     setWheel((prevWheel) => ({ ...prevWheel, rotation }));
   }, [wheel.theta]);
 
-  const snapToTop = useCallback((wheel) => {
-    const centerOfWheel = {
-      x:
-        wheelRef.current.getBoundingClientRect().x +
-        wheelRef.current.getBoundingClientRect().width / 2,
-      y:
-        wheelRef.current.getBoundingClientRect().y +
-        wheelRef.current.getBoundingClientRect().height / 2,
-    };
-
-    let shortestDistance = Infinity;
-    let closestItem = null;
-
-    // Loop through all items and compare distances to centerOfWheel; find shortest distance
-    Array.from(wheelRef.current.children).forEach((item) => {
-      const itemCenter = {
-        x:
-          item.getBoundingClientRect().x +
-          item.getBoundingClientRect().width / 2,
-        y:
-          item.getBoundingClientRect().y +
-          item.getBoundingClientRect().height / 2,
-      };
-
-      const dx = itemCenter.x - centerOfWheel.x;
-      const dy = itemCenter.y - centerOfWheel.y;
-      const currentDistance = Math.sqrt(dx * dx + dy * dy);
-
-      if (shortestDistance >= currentDistance) {
-        shortestDistance = currentDistance;
-        closestItem = item;
-      }
+  const handleScroll = useCallback((event) => {
+    clearTimeout(wheelStateRef.current.animationId);
+    const scrollSpeed = (event.deltaY / 360) * 20;
+    const tempTheta = wheelStateRef.current.tempTheta + scrollSpeed;
+    wheelRef.current.style.setProperty("transform", `rotate(${tempTheta}deg)`);
+    Array.from(wheelRef.current.children).forEach((child) => {
+      child.style.setProperty(
+        "transform",
+        `translate(-50%, -50%) rotate(${-1.0 * tempTheta}deg)`
+      );
     });
-
-    // Calculate the rotation needed to align the closest item to the top
-    const closestItemCenter = {
-      x:
-        closestItem.getBoundingClientRect().x +
-        closestItem.getBoundingClientRect().width / 2,
-      y:
-        closestItem.getBoundingClientRect().y +
-        closestItem.getBoundingClientRect().height / 2,
-    };
-
-    let closestItemTheta = Math.atan2(
-      closestItemCenter.y - centerOfWheel.y,
-      closestItemCenter.x - centerOfWheel.x
-    );
-    closestItemTheta = closestItemTheta * (180 / Math.PI) + 90; // Convert to degrees and adjust by 90 degrees to align with the top
-
-    // Adjust closestItemTheta to be within -90 to 90 degrees
-    if (closestItemTheta > 180) {
-      closestItemTheta -= 360;
-    }
-
-    let snapRotation = closestItemTheta - wheel.theta;
-    // Adjust snapRotation to be within -90 to 90 degrees
-    snapRotation = ((snapRotation + 90) % 180) - 90;
-
-    const newTheta = wheel.theta + snapRotation;
-    setWheel((prevWheel) => ({
-      ...prevWheel,
-      theta: newTheta,
-    }));
-    setRotation(newTheta);
-
-    // Adjust the transition for snapping
-    wheelRef.current.style.transition = "transform 0.5s"; // Adjust the time as needed
-
-    setTimeout(() => {
-      setIsPaused(false);
-    }, 500); // Adjust the delay as needed
+    const animationId = setTimeout(() => {
+      setWheel((prevWheel) => ({ ...prevWheel, theta: tempTheta }));
+    }, 150);
+    setWheel((prevWheel) => ({ ...prevWheel, animationId, tempTheta }));
   }, []);
 
-  const handleScroll = useCallback(
-    (event) => {
-      if (isPaused) {
-        return;
-      }
-
-      clearTimeout(wheelStateRef.current.animationId);
-      const scrollSpeed = (event.deltaY / 360) * 20;
-      const tempTheta = wheelStateRef.current.tempTheta + scrollSpeed;
-      wheelRef.current.style.setProperty(
-        "transform",
-        `rotate(${tempTheta}deg)`
-      );
-      Array.from(wheelRef.current.children).forEach((child) => {
-        child.style.setProperty(
-          "transform",
-          `translate(-50%, -50%) rotate(${-1.0 * tempTheta}deg)`
-        );
-      });
-      const animationId = setTimeout(() => {
-        setWheel((prevWheel) => {
-          const newWheel = { ...prevWheel, theta: tempTheta };
-          snapToTop(newWheel);
-          return newWheel;
-        });
-        setIsPaused(true); // Move setIsPaused(true) inside the timeout callback
-      }, 1000); // Increase the timeout value to 1000ms
-      setWheel((prevWheel) => ({ ...prevWheel, animationId, tempTheta }));
-    },
-    [isPaused, snapToTop]
-  );
-
-  const wheelStyle = {
-    transform: `rotate(${rotation}deg)`,
-    transition: "transform 0.5s ease-out",
-  };
-
   return (
-    <div
-      onWheel={handleScroll}
-      ref={wheelRef}
-      className={styles.wheel}
-      style={wheelStyle}
-    >
+    <div onWheel={handleScroll} ref={wheelRef} className={styles.wheel}>
       {loaded &&
         pokemonTypes.map((type, index) => {
           return (
