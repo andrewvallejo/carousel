2fb26f62db341f697b952e40da804f90826dd8c6
Andrew <andrew.a.vallejo@gmail.com>
2023-11-07 08:58:42 -0700

    Split scroll

--- src/app/components/Wheel.tsx
+++ src/app/components/Wheel.tsx
@@ -9,6 +9,7 @@ import { Type } from "components";
 import { multiplyByPi, pokemonTypes } from "utility";
 
 import styles from "./Wheel.module.scss";
+import useScroll from "../../hooks/useScroll";
 
 const initialWheelState = {
   radius: 360,
@@ -30,8 +31,6 @@ export function Wheel() {
     clearTimeout(wheelStateRef.current.animationId);
     const rotateSpeed = delta;
 
-    // We need to ensure our rotation snaps with a type at the top, not between types.
-    // As each type is separated by 20 degrees, we offset our rotation by 10 degrees
     let tempTheta =
       Math.round((wheelStateRef.current.tempTheta + rotateSpeed + 10) / 20) *
         20 -
@@ -51,37 +50,8 @@ export function Wheel() {
     setWheel((prevWheel) => ({ ...prevWheel, animationId, tempTheta }));
   }, []);
 
-  // variable to store the timestamp of the latest event
-  let lastEventTime = 0;
-  const throttleTime = 200; // define a buffering time of 200ms
-
-  const lastEventTimeRef = useRef(0);
-
-  const handleScroll = useCallback(
-    (event) => {
-      const currentTime = new Date().getTime(); // get the current time
-      if (currentTime - lastEventTimeRef.current > throttleTime) {
-        // check if enough time has passed since the last event
-        const rotateDegree = 20; // One type at a time
-        handleRotate(Math.sign(event.deltaY) * rotateDegree); // if so, we process the event
-        lastEventTimeRef.current = currentTime; // and update the timestamp of the latest event
-      }
-    },
-    [handleRotate]
-  );
-
-  const handleKeyDown = useCallback(
-    (event) => {
-      if (event.keyCode === 37) {
-        // Left Arrow Key
-        handleRotate(-20);
-      } else if (event.keyCode === 39) {
-        // Right Arrow Key
-        handleRotate(20);
-      }
-    },
-    [handleRotate]
-  );
+  // Use the useScroll hook here
+  useScroll(handleRotate);
 
   useEffect(() => {
     wheelStateRef.current = wheel;
@@ -94,15 +64,7 @@ export function Wheel() {
     const center = { x: width / 2, y: height / 2 };
     setWheel((prevWheel) => ({ ...prevWheel, center, radius }));
     setLoaded(true);
-
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("wheel", handleScroll);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("wheel", handleScroll);
-    };
-  }, [handleKeyDown, handleScroll]);
+  }, []);
 
   useEffect(() => {
     if (!wheelRef.current) return;
---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useCallback } from "react";
+
+/**
+ * ScrollHandler is a function type that will be invoked on scroll events.
+ * It accepts a number which will either be 20 or -20 based on the scroll direction.
+ */
+type ScrollHandler = (value: number) => void;
+
+/**
+ * useScroll is a custom React hook that triggers a function when a user scrolls.
+ *
+ * @param {ScrollHandler} func Function to be called upon scroll. It is passed a number indicating scroll direction (-20 or 20).
+ * @param {number} [throttleTime=200] The minimum wait time in milliseconds between each function execution.
+ */
+const useScroll = (func: ScrollHandler, throttleTime: number = 200) => {
+  const lastEventTimeRef = useRef(0);
+
+  /**
+   * handleScroll is a callback function that gets called upon scrolling.
+   * It calculates the rotation degree based on the scroll direction and executes the provided function.
+   * It also updates the reference time for throttling events.
+   *
+   * @param {React.WheelEvent} event The native DOM scroll event
+   */
+  const handleScroll = useCallback(
+    (event: React.WheelEvent) => {
+      const currentTime = new Date().getTime();
+
+      if (currentTime - lastEventTimeRef.current > throttleTime) {
+        const rotateDegree = 20;
+        func(Math.sign(event.deltaY) * rotateDegree);
+        lastEventTimeRef.current = currentTime;
+      }
+    },
+    [func, throttleTime]
+  );
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleScroll as any);
+    return () => {
+      window.removeEventListener("wheel", handleScroll as any);
+    };
+  }, [handleScroll]);
+};
+
+export default useScroll;
