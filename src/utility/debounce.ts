4f777dab67c5e1e8b6c8599eb9a3e84a2dd0a43c
Andrew <andrew.a.vallejo@gmail.com>
2023-11-29 16:23:01 -0700

    Add debouncer

---
+++ src/utility/debounce.ts
@@ -0,0 +1,9 @@
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
+  let timeoutId: NodeJS.Timeout | null = null;
+  return (...args: any[]) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
