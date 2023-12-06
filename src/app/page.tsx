5dc34f86b9140b9138d2c890071f7c2c3937e1ba
Andrew <andrew.a.vallejo@gmail.com>
2023-12-05 22:32:53 -0700

    Add experimental mobile detection feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,13 @@ import styles from "./page.module.scss";
 
 const initialType: PokemonType = "ice";
 
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
+window.dispatchEvent(new Event("resize"));
+
 export default function Home() {
   // TODO: create a global context for type selections
   const [selectedType, setSelectedType] = useState<PokemonType>(initialType);
