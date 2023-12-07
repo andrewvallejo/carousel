ea59ea422e7a74dedfa0d6b648afd299545c6220
Andrew <andrew.a.vallejo@gmail.com>
2023-12-06 22:51:18 -0700

    Fix server side window issue

--- src/hooks/useDetectMoblie.ts
+++ src/hooks/useDetectMoblie.ts
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 
 export const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 480);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    setIsMobile(window.innerWidth < 480);
     const checkIsMobile = () => setIsMobile(window.innerWidth < 480);
     window.addEventListener("resize", checkIsMobile);
     return () => window.removeEventListener("resize", checkIsMobile);
