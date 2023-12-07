10fa48cc4500ea2bc8289a847ebf01bdf4bb940f
Andrew <andrew.a.vallejo@gmail.com>
2023-12-06 22:35:43 -0700

    Split sets layout based on screen width

--- src/app/components/TypeSet.tsx
+++ src/app/components/TypeSet.tsx
@@ -4,6 +4,7 @@ import { TypeBlock } from "components/TypeBlock";
 import { calculateAllEffectiveness } from "utility/types";
 // Styles
 import styles from "./TypeSet.module.scss";
+import { useIsMobile } from "hooks/useDetectMoblie";
 
 interface TypeSetProps {
   types: PokemonType[];
@@ -12,25 +13,45 @@ interface TypeSetProps {
 }
 export function TypeSet({ types, currentType, flip }: TypeSetProps) {
   const typeMatrix = calculateAllEffectiveness(currentType);
+  const isMobile = useIsMobile();
 
-  const rows = [types.slice(0, 4), types.slice(4, 7), types.slice(7, 9)];
+  const mobileRows = [types.slice(0, 4), types.slice(4, 7), types.slice(7, 9)];
+  const rows = isMobile ? mobileRows : [types];
 
   return (
-    <div className={`${styles["type-set"]} ${flip ? styles["set-2"] : ""}`}>
-      {rows.map((row, index) => (
-        <ul
-          key={index}
-          className={`${styles.row} ${styles[`row-${index + 1}`]}`}
-        >
-          {row.map((type) => (
-            <TypeBlock
-              key={type}
-              type={type}
-              effectiveness={typeMatrix[type as keyof typeof typeMatrix]}
-            />
-          ))}
+    <div
+      className={`${styles["type-set"]} ${
+        flip && isMobile ? styles["set-2"] : ""
+      }`}
+    >
+      {isMobile ? (
+        mobileRows.map((row, index) => (
+          <ul
+            key={index}
+            className={`${styles.row} ${styles[`row-${index + 1}`]}`}
+          >
+            {row.map((type) => (
+              <TypeBlock
+                key={type}
+                type={type}
+                effectiveness={typeMatrix[type as keyof typeof typeMatrix]}
+              />
+            ))}
+          </ul>
+        ))
+      ) : (
+        <ul className={styles["type-set"]}>
+          {types.map((type) => {
+            return (
+              <TypeBlock
+                key={type}
+                type={type}
+                effectiveness={typeMatrix[type as keyof typeof typeMatrix]}
+              />
+            );
+          })}
         </ul>
-      ))}
+      )}
     </div>
   );
 }
